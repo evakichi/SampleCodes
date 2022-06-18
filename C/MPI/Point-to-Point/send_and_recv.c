a444568f79a83971190469628e9686187a39c54e
Makoto ISHIHARA <evakichi@be.wakwak.com>
2022-06-18 13:55:50 +0900

    modify.

--- C/MPI/Point-to-Point/send_and_recv.c
+++ C/MPI/Point-to-Point/send_and_recv.c
@@ -3,32 +3,28 @@
 
 int main(int argc,char *argv[])
 {
-    int A[3];
-    int rank,size;
-    MPI_Status status;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    if(rank==0)
-    {
-      A[0]=1;
-      A[1]=2;
-      A[2]=3;
-      printf("rank=0,before A(%d,%d,%d)\n",A[0],A[1],A[2]);
-      MPI_Send(A,3,MPI_INT,1,0,MPI_COMM_WORLD);
-      printf("rank=0,after  A(%d,%d,%d)\n",A[0],A[1],A[2]);
-    }
-    if(rank==1)
-    {
-      A[0]=0;
-      A[1]=0;
-      A[2]=0;
-      printf("rank=1,before A(%d,%d,%d)\n",A[0],A[1],A[2]);
-      MPI_Recv(A,3,MPI_INT,0,0,MPI_COMM_WORLD,&status);
-      printf("rank=1,after  A(%d,%d,%d)\n",A[0],A[1],A[2]);
-    }
-    MPI_Finalize();
-     
-   return 0;
+  int A[3]={0,0,0};
+  int rank,size;
+  MPI_Status status;
+
+  MPI_Init(&argc,&argv);
+
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  if(rank==0)
+    for(int i = 0 ; i < 3 ; i++)
+      A[i]=i+rank*3+1;
+
+  printf("rank=%d,before A(%d,%d,%d)\n",rank,A[0],A[1],A[2]);
+  if(rank==0)
+    MPI_Send(A,3,MPI_INT,1,0,MPI_COMM_WORLD);
+  if(rank==1)
+    MPI_Recv(A,3,MPI_INT,0,0,MPI_COMM_WORLD,&status);
+  printf("rank=%d,after  A(%d,%d,%d)\n",rank,A[0],A[1],A[2]);
+
+  MPI_Finalize();
+    
+  return 0;
 }
 
--- C/MPI/Point-to-Point/sendrecv_replace.c
+++ C/MPI/Point-to-Point/sendrecv_replace.c
@@ -3,35 +3,24 @@
 
 int main(int argc,char *argv[])
 {
-    int A[3];
-    int rank,size;
-    MPI_Status status;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    if(rank==0)
-    {
-      A[0]=1;
-      A[1]=2;
-      A[2]=3;
-    }
-    if(rank==1)
-    {
-      A[0]=4;
-      A[1]=5;
-      A[2]=6;
-    }     
-    if(rank==2)
-    {
-      A[0]=7;
-      A[1]=8;
-      A[2]=9;
-    }     
-      printf("rank=%d,before A(%d,%d,%d)\n",rank,A[0],A[1],A[2]);
-      MPI_Sendrecv_replace(A,3,MPI_INT,(rank+1)%size,rank,(rank+size-1)%size,(rank+size-1)%size,MPI_COMM_WORLD,&status);
-      printf("rank=%d,after  A(%d,%d,%d)\n",rank,A[0],A[1],A[2]);
-    MPI_Finalize();
-     
-   return 0;
+  int A[3]={0,0,0};
+  int rank,size;
+  MPI_Status status;
+
+  MPI_Init(&argc,&argv);
+
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  for(int i = 0 ; i < 3 ; i++)
+    A[i]=i+rank*3+1;
+
+  printf("rank=%d,before A(%d,%d,%d)\n",rank,A[0],A[1],A[2]);
+  MPI_Sendrecv_replace(A,3,MPI_INT,(rank+1)%size,rank,(rank+size-1)%size,(rank+size-1)%size,MPI_COMM_WORLD,&status);
+  printf("rank=%d,after  A(%d,%d,%d)\n",rank,A[0],A[1],A[2]);
+
+  MPI_Finalize();
+    
+  return 0;
 }
 
