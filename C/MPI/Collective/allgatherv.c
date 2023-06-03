983b811b94228012cb779fd88a84661124172a5a
Makoto ISHIHARA <evakichi@be.wakwak.com>
2023-06-03 19:52:19 +0900

    mofidy val for allgatherv.c .

--- C/MPI/Collective/allgatherv.c
+++ C/MPI/Collective/allgatherv.c
@@ -3,7 +3,7 @@
 
 int main(int argc,char *argv[])
 {
-  int A[3]={0,0,0},B[6]={0,0,0,0,0,0},S[3]={1,2,3},D[3]={0,1,3};
+  int A[3]={0,0,0},B[6]={0,0,0,0,0,0},R[3]={1,2,3},D[3]={0,1,3};
   int rank,size;
 
   MPI_Init(&argc,&argv);
@@ -28,7 +28,7 @@ int main(int argc,char *argv[])
   }
 
   printf("rank=%d,before A(%d,%d,%d),B(%d,%d,%d,%d,%d,%d)\n",rank,A[0],A[1],A[2],B[0],B[1],B[2],B[3],B[4],B[5]);
-  MPI_Allgatherv(A,rank+1,MPI_INT,B,S,D,MPI_INT,MPI_COMM_WORLD);
+  MPI_Allgatherv(A,rank+1,MPI_INT,B,R,D,MPI_INT,MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
   printf("rank=%d,after  A(%d,%d,%d),B(%d,%d,%d,%d,%d,%d)\n",rank,A[0],A[1],A[2],B[0],B[1],B[2],B[3],B[4],B[5]);
 
